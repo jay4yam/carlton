f5a7e80d11d6dc8c86482f2eb2f57b6edac3fae2
jayben <jay.ayamee@gmail.com>
2024-07-10 10:24:09 +0200

    add response to booking form

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,17 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::domain('carlton.localhost')->group(function () {
-    Route::get('/', [\App\Http\Controllers\HomeController::class, 'index'])->name('home');
-    Route::post('/booking/request', [\App\Http\Controllers\HomeController::class, 'bookingRequest'])->name('booking.request');
-    Route::get('thanks', [\App\Http\Controllers\HomeController::class, 'thanks'])->name('thanks');
-});
+if( env('APP_ENV') === 'production' ) {
+    Route::domain('carlton.michaelzingraf.com')->group(function () {
+        Route::get('/', [\App\Http\Controllers\HomeController::class, 'index'])->name('home');
+        Route::post('/booking/request', [\App\Http\Controllers\HomeController::class, 'bookingRequest'])->name('booking.request');
+        Route::get('thanks', [\App\Http\Controllers\HomeController::class, 'thanks'])->name('thanks');
+    });
+}else{
+    Route::domain('carlton.michaelzingraf.com')->group(function () {
+        Route::get('/', [\App\Http\Controllers\HomeController::class, 'index'])->name('home');
+        Route::post('/booking/request', [\App\Http\Controllers\HomeController::class, 'bookingRequest'])->name('booking.request');
+        Route::get('thanks', [\App\Http\Controllers\HomeController::class, 'thanks'])->name('thanks');
+    });
+}
+
