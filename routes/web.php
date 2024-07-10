e497401ae876ccf836fae9ca6e53751728b7a08a
jayben <jay.ayamee@gmail.com>
2024-07-10 10:00:23 +0200

    add response to booking form

--- routes/web.php
+++ routes/web.php
@@ -3,12 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::domain('carlton.michaelzingraf.com')->group(function () {
-    //Route::get('/', [\App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-    Route::get('/', function () {
-        dd('home page for subdomains');
-    });
-
+    Route::get('/', [\App\Http\Controllers\HomeController::class, 'index'])->name('home');
     Route::post('/booking/request', [\App\Http\Controllers\HomeController::class, 'bookingRequest'])->name('booking.request');
     Route::get('thanks', [\App\Http\Controllers\HomeController::class, 'thanks'])->name('thanks');
 });
